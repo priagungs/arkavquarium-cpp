fdaadd0b3a88bc094347c2b75a8c947bd430af94
shintaayuck <shinta990405@gmail.com>
2018-04-09 02:27:07 +0700

    Add files via upload

--- src versi 3 April/List.hpp
+++ src versi 3 April/List.hpp
@@ -1,108 +1,110 @@
-#ifndef LIST_H
-#define LIST_H
-#define DEFSIZE 2
+#include <iostream>
 
-template<class Type>
-class List{
-//Kelas generik
-//Direpresentasikan sebagai array of Type
-
-public:
-    //Ctor
-    List(){
-        elements = new Type[DEFSIZE];
-        idxlast = -1;
-    }
-    //Dtor
-    ~List(){
-        delete [] elements;
-    }
-    //CCTOR
-    List(const List<Type>& L){
-        idxlast = L.idxlast;
-        elements = new Type[DEFSIZE];
-        for(int i=0; i<=idxlast; i++){
-            elements[i] = L.elements[i];
-        }
-    }
-    //Assignment
-    List& operator=(const List<Type>& L){
-        idxlast = L.idxlast;
-        elements = new Type[DEFSIZE];
-        for(int i=0; i<=idxlast; i++){
-            elements[i] = L.elements[i];
-        }
-        return *this;
-    }
-    //Mengembalikan indeks dimana element ditemukan
-    int find(Type element){
-        for(int i=0; i<=idxlast; i++){
-            if(element == elements[i]){
-                return i;
-            }
-        }
-        return -1;
-    }
-    //Mengembalikan true jika list kosong
-    bool isEmpty(){
-        return idxlast == -1;
-    }
-    //Menambahkan element pada list
-    void add(Type element){
-        if(idxlast+1 == DEFSIZE){
-            // copy element elements to temporary
-            Type* temp = new Type[DEFSIZE];
-            for(int i=0; i<=idxlast; i++){
-                temp[i] = elements[i];
-            }
-
-            // delete old elements
-            delete [] elements;
-
-            // create new elements with size 2*size of old elements
-            elements = new Type[(idxlast+1)*2];
-
-            // copy everything in temp to new elements
-            for(int i=0; i<=idxlast; i++){
-                elements[i] = temp[i];
-            }
-
-            // delete temp;
-            delete [] temp;
-
-            // add new element in the last
-            idxlast++;
-            elements[idxlast] = element;
-        }
-        else{
-            idxlast++;
-            elements[idxlast] = element;
-        }
-    }
-    //Menghapus element pada list
-    void remove(Type element){
-        int removedIdx;
-        for(int i=0; i<=idxlast; i++){
-            if(elements[i] == element){
-                removedIdx = i;
-                break;
-            }
-        }
-        for(int i=removedIdx+1; i<=idxlast; i++){
-            elements[i-1] = elements[i];
-        }
-        idxlast--;
-    }
-
-    //Mengambil elemen pada index tertentu
-    Type get(int index){
-        return elements[index];
-    }
-
-
-private:
-    Type* elements; //representasi list sebagai array of Type
-    int idxlast; //Indeks array dimana elemen list terakhir
+template<class T>
+struct elmt {
+	elmt<T>* next;
+	T info;
 };
 
-#endif
+template<class T>
+class List
+{
+public:
+	elmt<T>* first;	//Pointer ke elemen pertama
+	elmt<T>* last;	//Pointer ke elemen terakhir
+	
+	//CTOR
+	List<T>() { 
+		first = NULL;
+		last = NULL;
+	}
+	
+	bool isEmpty() { 	//Mengembalikan true jika list kosong
+		return (first==NULL && last == NULL);
+	}
+	
+	//Menambah elemen T ke dalam list
+	void add(T info) {
+		if(!first) { 	//Kondisi list kosong
+			first = new elmt<T>;
+			first->info = info;
+			first->next = NULL;
+			last = first;
+		} else { 		//Kondisi list tidak kosong
+			if(last == first) { //Satu elemen
+				last = new elmt<T>;
+				last->info = info;
+				last->next = NULL;
+				first->next = last;
+			} else { 	//Kondisi list memiliki lebih dari satu elemen
+				elmt<T>* insinfo = new elmt<T>;
+				insinfo->info = info;
+				insinfo->next = NULL;
+				last->next = insinfo;
+				last = insinfo;
+			}
+		}
+	}
+
+    //Menghapus elemen T pada list
+    void remove(T element){
+		if (first->info == element) {	//elemen ada di elemen pertama
+			elmt<T>* del = first;
+			if (first-> next != NULL) {	//list memiliki lebih dari satu elemen
+				elmt<T>* temp = first->next;
+				first->info = first->next->info;
+				first->next = first->next->next;				
+			} else {					//list memiliki satu elemen
+				first = NULL;
+				last = NULL;
+			}
+			delete del;
+		} else {						//elemen tidak di elemen pertama
+			elmt<T>* current = first;
+			while (current->next->info != element && current->next != NULL) {
+				//Iterasi sampai elemen terakhir atau sampai elemen ditemukan
+				current = current->next;
+			}
+			if (current->next != NULL) { //Jika elemen ditemukan, dihapus dari list
+				elmt<T>* del = current;
+				current->next = current->next->next;
+				delete del;
+			}
+		}
+    }
+
+	T get(int index) {		//Mengembalikan elemen di indeks tertentu
+		if(index == 0) {	//Ambil elemen pertama 
+			return this->first->info;
+		} else {			//Mencari elemen dengan indeks yang tepat
+			elmt<T>* curr = this->first;
+			for(int i = 0; i < index; ++i) {
+				curr = curr->next;
+			}
+			return curr->info;
+		}
+	}
+	
+	int find(T element) {				//Mengembalikan indeks terkecil tempat T ditemukan
+		if (first->info == element) {	//Kondisi elemen ditemukan di elemen pertama
+			return 0;
+		} else {
+			int counter = 0;			//Mencari elemen yang sesuai
+			elmt<T>* current = this->first;
+			while (current->next != NULL && current->next->info != element) {
+				counter++;
+				current = current->next;
+			}
+			if (current->next != NULL) {	
+				//Kondisi perulangan berhenti karena elemen ditemukan, mengembalikan indeks
+				return counter;
+			} else {
+				//elemen tidak ditemukan, mengembalikan nilai -1
+				return -1;
+			}
+		}
+	}
+
+	
+
+};
